10bc851df379e013c00f1e4d1920f211c94859c9
TianLi <tianli@TianLideiMac.local>
2018-04-13 15:20:44 +0800

    Initial Commit

---
+++ 日历开发/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  日历开发
+//
+//  Created by TianLi on 2018/4/13.
+//  Copyright © 2018年 TianLi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
